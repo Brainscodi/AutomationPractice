65c882ea88566d8ed47ee7dc4e09f4288571b371
Brainscodi <codingbrains20@gmail.com>
2024-04-16 20:49:37 +0530

    Login
    This is login code

--- src/test/java/com/eva/registration/Registration_Login.java
+++ src/test/java/com/eva/registration/Registration_Login.java
@@ -1,5 +1,7 @@
 package com.eva.registration;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,19 +14,26 @@ import net.bytebuddy.utility.RandomString;
 
 public class Registration_Login {
 	private WebDriver driver=null;
-	@Test()
-	public void registration()
+	public void commoncode()
 	{
 		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 		driver.get("https://automationexercise.com/");
 		String actualhomepagetitle=  driver.findElement(By.xpath("//i[@class='fa fa-home']/parent::a")).getText();
 		Assert.assertEquals(actualhomepagetitle, "Home");
-		driver.findElement(By.xpath("//a[@href='/login']")).click();
+		driver.findElement(By.xpath("//a[@href='/login']")).click();	
+	}
+
+
+	@Test(enabled = true)
+	public void registration()
+	{    
+		commoncode();
 		String actualnewuserSignup=	driver.findElement(By.xpath("//h2[text()='New User Signup!']")).getText();;
 		Assert.assertEquals(actualnewuserSignup, "New User Signup!");
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Kishan");
 		String random=	  new RandomString().make(2);
-		driver.findElement(By.xpath("//input[@placeholder='Name']/following-sibling::input[@placeholder='Email Address']")).sendKeys("kishan@yopmail.com"+random);
+		driver.findElement(By.xpath("//input[@placeholder='Name']/following-sibling::input[@placeholder='Email Address']")).sendKeys("codingkishan2001@gmail.com");
 		driver.findElement(By.xpath("//button[text()='Signup']")).click();
 		driver.findElement(By.id("id_gender1")).click();
 		driver.findElement(By.id("password")).sendKeys("Kishan@12345");
@@ -58,10 +67,54 @@ public class Registration_Login {
 		String loginkishan=  driver.findElement(By.xpath("//b[text()='Kishan']")).getText();
 		Assert.assertEquals(loginkishan, "Kishan");
 		driver.findElement(By.xpath("//a[@href='/delete_account']")).click();
-     String accountdelete=   driver.findElement(By.xpath("//b[text()='Account Deleted!']")).getText();
-    Assert.assertEquals(accountdelete, "ACCOUNT DELETED!");
-
+		String accountdelete=   driver.findElement(By.xpath("//b[text()='Account Deleted!']")).getText();
+		Assert.assertEquals(accountdelete, "ACCOUNT DELETED!");
+		driver.quit();
 
 	}
+    @Test(enabled = false)
+	public void validlogin()
+	{
+		commoncode();
+		String loginToYourAccount=driver.findElement(By.xpath("//h2[text()='Login to your account']")).getText();
+        Assert.assertEquals(loginToYourAccount, "Login to your account");
+        driver.findElement(By.xpath("//input[@name='password']/preceding-sibling::input/following-sibling::input[@name='email']")).sendKeys("kishan@yopmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Kishan@12345");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+        String loginkishan=  driver.findElement(By.xpath("//b[text()='Kishan']")).getText();
+		Assert.assertEquals(loginkishan, "Kishan");
+		driver.findElement(By.xpath("//a[@href='/delete_account']")).click();
+		String accountdelete=   driver.findElement(By.xpath("//b[text()='Account Deleted!']")).getText();
+		Assert.assertEquals(accountdelete, "ACCOUNT DELETED!");
+
+	}
+    @Test(enabled = false)
+    public void invalidLogin()
+    {
+    	commoncode();
+		String loginToYourAccount=driver.findElement(By.xpath("//h2[text()='Login to your account']")).getText();
+        Assert.assertEquals(loginToYourAccount, "Login to your account");
+        driver.findElement(By.xpath("//input[@name='password']/preceding-sibling::input/following-sibling::input[@name='email']")).sendKeys("khan@yopmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Kishan@123");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+        String actualincorrecttext=   driver.findElement(By.xpath("//p[text()='Your email or password is incorrect!']")).getText();
+        Assert.assertEquals(actualincorrecttext, "Your email or password is incorrect!");
+     
+    }
+    @Test(priority = 2)
+    public void logout()
+    {
+    	commoncode();
+		String loginToYourAccount=driver.findElement(By.xpath("//h2[text()='Login to your account']")).getText();
+        Assert.assertEquals(loginToYourAccount, "Login to your account");
+        driver.findElement(By.xpath("//input[@name='password']/preceding-sibling::input/following-sibling::input[@name='email']")).sendKeys("codingkishan2001@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Kishan@12345");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+        String loginkishan=  driver.findElement(By.xpath("//b[text()='Kishan']")).getText();
+		Assert.assertEquals(loginkishan, "Kishan");
+		driver.findElement(By.xpath("//a[@href='/logout']")).click();
+		String navigateloginpage=driver.findElement(By.xpath("//h2[text()='Login to your account']")).getText();
+        Assert.assertEquals(navigateloginpage, "Login to your account");
+    }
 
 }
